51919eaa91b210f5c1eef0867fbb8d3dc3de92f7
Nedko Stefanov <nedkospetc@gmail.com>
2023-03-14 20:23:26 +0200

    adding sample styles for the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,68 @@
-function App() {
-	return <div className="App"></div>;
-}
+import React, { useState, useEffect } from "react";
+import wallpaper from "./assets/images/weather-wallpapers.jpg";
+
+const App = () => {
+	const [response, setResponse] = useState({});
+
+	return (
+		<div className="App">
+			<div className="card">
+				<div className="card__image">
+					<img src={wallpaper} alt="" />
+				</div>
+
+				<div className="card__content">
+					<h3>Varna</h3>
+
+					<h2>34F</h2>
+
+					<div className="span">Clouds</div>
+				</div>
+			</div>
+
+			<ul className="tiles">
+				<li>
+					<div className="tile">
+						<h6>34f</h6>
+
+						<p>Monday</p>
+					</div>
+				</li>
+
+				<li>
+					<div className="tile">
+						<h6>34f</h6>
+
+						<p>Monday</p>
+					</div>
+				</li>
+
+				<li>
+					<div className="tile">
+						<h6>34f</h6>
+
+						<p>Monday</p>
+					</div>
+				</li>
+
+				<li>
+					<div className="tile">
+						<h6>34f</h6>
+
+						<p>Monday</p>
+					</div>
+				</li>
+
+				<li>
+					<div className="tile">
+						<h6>34f</h6>
+
+						<p>Monday</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+	);
+};
 
 export default App;
