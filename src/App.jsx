3c94691cfb34082be364f33ba0583259489b23c6
Nedko Stefanov <nedkospetc@gmail.com>
2023-03-19 20:51:46 +0200

    Adding functionality that takes the user's location.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,31 @@ import React, { useState, useEffect } from "react";
 
 import Card from "./components/card";
 import Tiles from "./components/tiles";
-
 import wallpaper from "./assets/images/weather-wallpapers.jpg";
-
 import handleAjax from "./helpers/handle-ajax";
 
 const App = () => {
 	const [data, setData] = useState({});
+	const [userLocation, setUserLocation] = useState([]);
+	const baseUrl = "https://api.openweathermap.org/data/2.5/forecast?";
 
 	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(function (position) {
+			const lat = position.coords.latitude;
+			const long = position.coords.longitude;
+			const newUserLocation = [lat, long];
+			setUserLocation(newUserLocation);
+		});
+	}, []);
+
+	useEffect(() => {
+		const url =
+			userLocation.length > 0
+				? `${baseUrl}lat=${userLocation[0]}&lon=${userLocation[1]}&appid=acdef770b69db9dd76eee3d335a25eb9&units=metric&cnt`
+				: `${baseUrl}lat=42.696993&lon=23.310437&appid=acdef770b69db9dd76eee3d335a25eb9&units=metric&cnt`;
+
 		handleAjax(
-			"https://api.openweathermap.org/data/2.5/forecast?lat=43.219816&lon=27.903659&appid=acdef770b69db9dd76eee3d335a25eb9&units=metric&cnt",
+			url,
 			function (status, error) {
 				console.log(error);
 				return;
@@ -22,7 +36,7 @@ const App = () => {
 				setData(newData);
 			}
 		);
-	}, []);
+	}, [userLocation]);
 
 	return (
 		<div className="App">
